81a173acd5634ddb8fcc297cedb2e58dce8e91c7
Orlando Buzana <orlandobuzana@outlook.com>
2019-04-01 01:19:59 -0400

    removed tbs, layout positioning using css

---
+++ testingnode.js
@@ -0,0 +1,16 @@
+console.log('hello');
+var primenumber = 7;
+if (primenumber > 7){
+
+    console.log('This is lesser motherfucker');
+}
+else if(primenumber == 7){
+    console.log('same number');
+}
+var person = {Name:"orlando", Id:"34"};
+console.log(person.Name+" this is the man");
+
+for (i = 0; i < 5; i++){
+    
+    console.log(i);
+}
